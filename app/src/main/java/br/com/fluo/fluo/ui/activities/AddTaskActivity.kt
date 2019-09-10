88b8191cb96229962bef96db1c3440a962fc12fa
Cotemig <cotemig@imac51.lan>
2019-09-09 21:54:36 -0300

    Criando picker

--- app/src/main/java/br/com/fluo/fluo/ui/activities/AddTaskActivity.kt
+++ app/src/main/java/br/com/fluo/fluo/ui/activities/AddTaskActivity.kt
@@ -1,9 +1,12 @@
 package br.com.fluo.fluo.ui.activities
 
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import br.com.fluo.fluo.R
+import br.com.fluo.fluo.helper.DateTime
 import br.com.fluo.fluo.models.Project
 import br.com.fluo.fluo.services.RetrofitInitializer
 import br.com.fluo.fluo.ui.adapters.HorizontalProjectsAdapter
@@ -19,10 +22,47 @@ class AddTaskActivity : AppCompatActivity(), HorizontalProjectsAdapter.ProjectVi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_task)
+
         getProjects()
+
         closetask.setOnClickListener {
             finish()
         }
+
+        selectdate.setOnClickListener {
+            showPickerDate()
+        }
+    }
+
+    fun showPickerDate() {
+
+        var now = DateTime.now()
+
+        val dpd = DatePickerDialog(
+            this,
+            DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+
+                showPickerTime()
+
+            }, now.year, now.month - 1, now.day
+        )
+
+        dpd.show()
+
+    }
+
+    fun showPickerTime() {
+
+        var now = DateTime.now()
+
+        val tpd = TimePickerDialog(this,
+            TimePickerDialog.OnTimeSetListener { view, hourOfDay, minute ->
+
+            }, now.hour, now.minute, false
+        )
+
+        tpd.show()
+
     }
 
     fun getProjects() {
@@ -44,7 +84,8 @@ class AddTaskActivity : AppCompatActivity(), HorizontalProjectsAdapter.ProjectVi
                             LinearLayoutManager(this@AddTaskActivity, LinearLayoutManager.HORIZONTAL, false)
                         projects.layoutManager = layoutManager
 
-                        adapter = HorizontalProjectsAdapter(this@AddTaskActivity, listprojects, -1, this@AddTaskActivity)
+                        adapter =
+                            HorizontalProjectsAdapter(this@AddTaskActivity, listprojects, -1, this@AddTaskActivity)
 
                         projects.adapter = adapter
 
